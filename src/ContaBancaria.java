c5e7b73c2f761bd7d71308284192906878f5c8ac
fic <fic@DESKTOP-0L5VRFL>
2025-08-13 21:52:58 -0300

    Fundamentos POO Java

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -2,5 +2,14 @@ public class ContaBancaria {
     public String nome;
     public double saldo;
 
+    public void depositar(double depositarValor){
+        saldo += depositarValor;
+        System.out.println("Valor atual: R$" + saldo);
+    }
 
+    public void sacar(double sacarValor){
+        saldo -= sacarValor;
+        System.out.println("Valor sacado: R$" + sacarValor + "\nSaldo Atual: R$" + saldo);
+
+    }
 }
